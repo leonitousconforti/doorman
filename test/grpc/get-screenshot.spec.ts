8e34e664a7df037027e12dd7d35bafb2da605649
Leo Conforti <leo@leoconforti.us>
2022-06-24 04:40:54 +0000

    update get-screenshot tests with better messages

--- test/grpc/get-screenshot.spec.ts
+++ test/grpc/get-screenshot.spec.ts
@@ -11,23 +11,31 @@ const clientWithTinyReceive = createEmulatorControllerClient(emulatorAddress, un
 const clientWithLargerReceive = createEmulatorControllerClient(emulatorAddress, undefined, largeReceiveOptions);
 
 // Fails to get a screenshot because it is too much data
-test("Should fail to get a screenshot", async (t) => {
+test("Should fail to get a screenshot when receive buffer is too small", async (t) => {
     t.rejects(getScreenshot(clientWithTinyReceive));
 });
 
 // Get a screenshot with the default options
 test("Should get a screenshot in rgb format", async (t) => {
     const screenshot = await getScreenshot(clientWithLargerReceive);
-    t.equal(screenshot.channels, 3);
-    t.equal(screenshot.format, ImageType.RGB);
-    t.equal(screenshot.pixels.length, screenshot.width * screenshot.height * screenshot.channels);
+    t.equal(screenshot.channels, 3, "expect three channels");
+    t.equal(screenshot.format, ImageType.RGB, "image format should be rgb");
+    t.equal(
+        screenshot.pixels.length,
+        screenshot.width * screenshot.height * screenshot.channels,
+        "pixel buffer should be correct size"
+    );
 });
 
 // Get a screenshot with non-default options
 test("Should get a screenshot in rgba format", async (t) => {
     const ImageOptionsWithAlpha: ImageFormat = { format: "RGBA8888" };
     const screenshot = await getScreenshot(clientWithLargerReceive, ImageOptionsWithAlpha);
-    t.equal(screenshot.channels, 4);
-    t.equal(screenshot.format, ImageType.RGB_A);
-    t.equal(screenshot.pixels.length, screenshot.width * screenshot.height * screenshot.channels);
+    t.equal(screenshot.channels, 4, "expect four channels");
+    t.equal(screenshot.format, ImageType.RGB_A, "image format should be rgb_a");
+    t.equal(
+        screenshot.pixels.length,
+        screenshot.width * screenshot.height * screenshot.channels,
+        "pixel buffer should be correct size"
+    );
 });
